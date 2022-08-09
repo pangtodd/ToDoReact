7ce7838068674c603be3c90d10b6c15f84d527a0
Todd Pangilinan <pang.todd@gmail.com>
2022-08-09 13:48:21 -0700

    added photo to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import toDoImage from "./../img/todo.png";
 
 function Header(){
   return (
-    <h1>TO DOs!</h1>
+    <React.Fragment>
+      <h1>TO DOs!</h1>
+      <img src={toDoImage} alt="guy checking to-dos off" />
+    </React.Fragment>
   );
 }
 

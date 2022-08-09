35ca31bacd59e6b582e02b09e5d9de24d21d2a11
Todd Pangilinan <pang.todd@gmail.com>
2022-08-08 22:50:45 -0700

    establish initial ToDo.js file.

---
+++ src/components/ToDo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ToDo(){
+  const name = "Todd";
+  const name2 = "Pangilinan";
+  return(
+    <React.Fragment>
+      <h3>8:00am</h3>
+      <h3>Wake Up!</h3>
+      <p><em>Start a new day already.</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default ToDo;

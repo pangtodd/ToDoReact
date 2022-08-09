56673684207dcac5daf43b2c0f349bc12f01a761
Todd Pangilinan <pang.todd@gmail.com>
2022-08-08 23:11:59 -0700

    updated ToDo.js to include propTypes.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,16 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ToDo(){
-  const name = "Todd";
-  const name2 = "Pangilinan";
+function ToDo(props){
   return(
     <React.Fragment>
-      <h3>8:00am</h3>
-      <h3>Wake Up!</h3>
-      <p><em>Start a new day already.</em></p>
+      <h3>{props.date} - {props.time}</h3>
+      <h3>{props.location}</h3>
+      <p><em>{props.description}</em></p>
       <hr/>
     </React.Fragment>
   );
 }
+
+ToDo.propTypes = {
+  date: PropTypes.string,
+  time: PropTypes.string,
+  location: PropTypes.string,
+  description: PropTypes.string
+};
 
 export default ToDo;

10879272a18a15ffb61b8d6036fb8056371f29eb
Todd Pangilinan <pang.todd@gmail.com>
2022-08-10 08:32:42 -0700

    established initial ToDoDetail.js

---
+++ src/components/ToDoDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ToDoDetail(props){
+  return (
+    <React.Fragment>
+      <h1>To Do detail</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default ToDoDetail;
